d43ca79ea064b907edca1b9389d02e20f910c4fb
MizuNotCool <suzudevelopment@gmail.com>
2022-07-14 11:47:42 +0800

    test only

---
+++ BoardConfig.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from samsung mt6765-common
+-include device/samsung/mt6765-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/samsung/a10s
+
+# Board
+TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
+
+# Kernel
+TARGET_KERNEL_CONFIG := a10s_defconfig
+
+# OTA
+TARGET_OTA_ASSERT_DEVICE := a10s
---
+++ lineage_a10s.mk
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from device
+$(call inherit-product, device/xiaomi/cactus/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := a10s
+PRODUCT_NAME := lineage_a10s
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := Galaxy A10s
+PRODUCT_MANUFACTURER := Samsung
+
+# Override fingerprint from stock
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="a10sxx-user 10 QP1A.190711.020 A107FXXU8BUC2 release-keys" \
+    PRODUCT_NAME="a10s" \
+    TARGET_DEVICE="a10s"
+
+BUILD_FINGERPRINT := samsung/a10sxx/a10s:10/QP1A.190711.020/A107FXXU8BUC2:user/release-keys
