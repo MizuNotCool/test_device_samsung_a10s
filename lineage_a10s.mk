5b51fa0ef7adc5c00ba2833c81a8f4d7a2a7caea
ItzKaguya 2nd <106611054+MizuNotCool@users.noreply.github.com>
2022-07-17 10:25:01 +0800

    Update lineage_a10s.mk

--- lineage_a10s.mk
+++ lineage_a10s.mk
@@ -15,7 +15,7 @@
 #
 
 # Inherit from device
-$(call inherit-product, device/Samsung/a10s/device.mk)
+$(call inherit-product, device/samsung/a10s/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
